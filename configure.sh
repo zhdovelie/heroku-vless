46598467544f6fca280d19583b53517bb460d924
Dimitri2020007 <72486732+Dimitri2020007@users.noreply.github.com>
2021-02-20 12:20:14 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -31,7 +31,7 @@ cat << EOF > /usr/local/etc/xray/config.json
             "streamSettings": {
                 "network": "ws",
                 "wsSettings": {
-                  "path": "/$ID-trojan"
+                  "path": "/$ID-vless"
                 }
             }
         }
