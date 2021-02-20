d98d695318c307835900a0085fddb73252726f57
Dimitri2020007 <72486732+Dimitri2020007@users.noreply.github.com>
2021-02-20 10:49:43 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,29 +1,26 @@
 #!/bin/sh
 
-# Download and install V2Ray
-mkdir /tmp/v2ray
-curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
-unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
-install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
-install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
+# Download and install XRay
+mkdir /tmp/xray
+curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
+unzip /tmp/xray/xray.zip -d /tmp/xray
+install -m 755 /tmp/xray/xray /usr/local/bin/xray
 
 # Remove temporary directory
-rm -rf /tmp/v2ray
+rm -rf /tmp/xray
 
 # V2Ray new configuration
-install -d /usr/local/etc/v2ray
-cat << EOF > /usr/local/etc/v2ray/config.json
+install -d /usr/local/etc/xray
+cat << EOF > /usr/local/etc/xray/config.json
 {
     "inbounds": [
         {
-            "port": $PORT,
-            "protocol": "vless",
+            "port": 8080,
+            "protocol": "trojan",
             "settings": {
                 "clients": [
                     {
                         "id": "$ID", 
-                        "flow": "xtls-rprx-direct",
-                        "level": 0,
                         "email": "love@v2fly.org"
                     }
                 ],
@@ -31,6 +28,9 @@ cat << EOF > /usr/local/etc/v2ray/config.json
             },
             "streamSettings": {
                 "network": "ws"
+                "wsSettings": {
+                  "path": "/${XR_TR_UUID}-trojan"
+                }
             }
         }
     ],
@@ -42,5 +42,5 @@ cat << EOF > /usr/local/etc/v2ray/config.json
 }
 EOF
 
-# Run V2Ray
-/usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json
+# Run XRay
+/usr/local/bin/xray -config /usr/local/etc/xray/config.json
