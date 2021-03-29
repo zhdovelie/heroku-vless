465e67b63d267d70d7611cd2a6289a5ff5ea0246
Dimitri2020007 <72486732+Dimitri2020007@users.noreply.github.com>
2021-03-29 19:09:30 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -33,7 +33,7 @@ cat << EOF > /usr/local/etc/xray/config.json
                 "network": "ws",
                 "allowInsecure": false,
                 "wsSettings": {
-                  "path": "/$ID-vless?ed=2048"
+                  "path": "/$ID-vless"
                 }
             }
         }
