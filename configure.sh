6439bb4b7695cd838a5a52108795106e522fddcc
Dimitri2020007 <72486732+Dimitri2020007@users.noreply.github.com>
2021-03-29 12:57:33 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -16,6 +16,8 @@ cat << EOF > /usr/local/etc/xray/config.json
 {
     "inbounds": [
         {
+            "port": 8080,
+            "listen": "127.0.0.1",
             "protocol": "vless",
             "settings": {
                 "clients": [
