214c52755978e8c3060d9fce5117f04e1b859472
Dimitri2020007 <72486732+Dimitri2020007@users.noreply.github.com>
2021-04-03 13:54:55 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -16,8 +16,7 @@ cat << EOF > /usr/local/etc/xray/config.json
 {
     "inbounds": [
         {        
-            "listen": "0.0.0.0",
-            "port": 8080,
+            "listen": "/etc/caddy/vless",
             "protocol": "vless",
             "settings": {
                 "clients": [
@@ -50,7 +49,7 @@ EOF
 # Config Nginx
 mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
-wget https://raw.githubusercontent.com/Dimitri2020007/heroku-vless/main/etc/nginx.conf | sed -e "1c :$PORT" -e "s/\$ID/$ID/g" >/etc/nginx/nginx.conf
+wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$ID/$ID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $ID)/g" >/etc/caddy/Caddyfile
 
 # Run XRay
-tor & /usr/local/bin/xray -config /usr/local/etc/xray/config.json & nginx start
+tor & /usr/local/bin/xray -config /usr/local/etc/xray/config.json & caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
