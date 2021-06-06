8877fc42db6138dd2eaeb1ebf5ed6e885ee32386
Dimitri2020007 <72486732+Dimitri2020007@users.noreply.github.com>
2021-06-06 23:19:43 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -22,7 +22,7 @@ cat << EOF > /usr/local/etc/xray/config.json
                 "clients": [
                     {
                         "id": "$ID", 
-                        "flow": "xtls-rprx-direct-udp443",
+                        "flow": "xtls-rprx-direct",
                         "level": 0,
                         "email": "love@v2fly.org"
                     }
