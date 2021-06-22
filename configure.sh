e31edef638188fc9b9a7cca9bbc6641c24c7bda0
Dimitri2020007 <72486732+Dimitri2020007@users.noreply.github.com>
2021-06-22 19:36:28 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -53,14 +53,9 @@ cat << EOF > /usr/local/etc/xray/config.json
 EOF
 
 # Config Caddy
-sed -e "/^#/d"\
-    -e "1c :$PORT"
-    -e "s/\$ID/$ID/g" 
-    -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $ID)/g"
-    -e "$s"\
-    /etc/Caddyfile > /etc/caddy/Caddyfile
-echo /etc/caddy/Caddyfile
-cat /etc/caddy/Caddyfile
+mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
+wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
+wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$ID/$ID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $ID)/g" >/etc/caddy/Caddyfile
 
 # Run XRay
 tor & /usr/local/bin/xray -config /usr/local/etc/xray/config.json & nginx -g 'daemon off;'
