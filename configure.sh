3dccf4132250c4eb6503a50964fc1dcf1bdb3b2d
Dimitri2020007 <72486732+Dimitri2020007@users.noreply.github.com>
2021-07-11 18:50:51 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -19,13 +19,13 @@ cat << EOF > /usr/local/etc/xray/config.json
     },
     "inbounds": [
         {   
-            "port": 8180
-            "listen": "0.0.0.0",
+            "listen": "/etc/caddy/vless",
             "protocol": "vless",
             "settings": {
                 "clients": [
                     {
                         "id": "$ID", 
+                        "flow": "xtls-rprx-direct",
                         "level": 0,
                         "email": "love@v2fly.org"
                     }
@@ -33,11 +33,10 @@ cat << EOF > /usr/local/etc/xray/config.json
                 "decryption": "none"
             },
             "streamSettings": {
-                "network": "grpc",
+                "network": "ws",
                 "allowInsecure": false,
-                "grpcSettings": {
-                  "serviceName": "grpc",
-                  "multiMode": true
+                "wsSettings": {
+                  "path": "/$ID-vless?ed=2048"
                 }
             }
         }
