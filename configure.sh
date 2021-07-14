b4334cf9d904752f047d627396451d94a34e0725
Dimitri2020007 <72486732+Dimitri2020007@users.noreply.github.com>
2021-07-14 18:23:21 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -59,10 +59,5 @@ mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
 wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$APPNAME/$APPNAME/g" -e "s/\$ID/$ID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $ID)/g" >/etc/caddy/Caddyfile
 
-# Config ACME
-acme.sh --issue -d $APPNAME.herokuapp.com --webroot /usr/share/caddy
-acme.sh --install-cert -d $APPNAME.herokuapp.com --cert-file /usr/share/caddy/cert.crt --key-file /usr/share/caddy/cert.key
-acme.sh --upgrade --auto-upgrade
-
 # Run XRay
 tor & /usr/local/bin/xray -config /usr/local/etc/xray/config.json & caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
