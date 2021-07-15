58aa89ea1fd93976eaebc369aea0e18f7c1a5918
Dimitri2020007 <72486732+Dimitri2020007@users.noreply.github.com>
2021-07-15 11:45:06 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -59,5 +59,9 @@ mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
 wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$ID/$ID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $ID)/g" >/etc/caddy/Caddyfile
 
+# Config Openssl
+cd /usr/share/caddy
+openssl x509 -req -sha256 -days 365 -in server.csr -signkey cert.key -out cert.crt
+
 # Run XRay
 tor & /usr/local/bin/xray -config /usr/local/etc/xray/config.json & caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
