8d652b81ed738f4e9b1a124c488e8ead452c1954
Dimitri2020007 <72486732+Dimitri2020007@users.noreply.github.com>
2021-07-16 13:27:00 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -48,8 +48,8 @@ cat << EOF > /usr/local/etc/xray/config.json
     ],
     "dns": {
         "servers": [
-            "1.1.1.1",
-            "8.8.8.8"
+            "https://cloudflare-dns.com/dns-query",
+            "https://dns.google/dns-query"
         ]
     }
 }
