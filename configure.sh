1c0ce653028d7cda0f07fbc2c2cbda266c50f59b
Dimitri2020007 <72486732+Dimitri2020007@users.noreply.github.com>
2021-07-16 15:57:05 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -39,11 +39,10 @@ cat << EOF > /usr/local/etc/v2ray/config.json
                   "path": "/$ID-vless?ed=2048"
                 },
                 "tlsSettings": {
-                  "serverName": "$APPNAME.herokuapp.com",
                   "certificates": [
                       "certificateFile": "/usr/share/caddy/cert.crt",
                       "keyFile": "/usr/share/caddy/cert.key"
-                  ],
+                  ]
                 }
             }
         }
