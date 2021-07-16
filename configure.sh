7c4e84a8c72a67156f3632c3f9ead7cadd31ba0e
Dimitri2020007 <72486732+Dimitri2020007@users.noreply.github.com>
2021-07-16 16:17:06 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -5,6 +5,7 @@ mkdir /tmp/v2ray
 curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
 unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
 install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
+install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
 v2ctl cert -ca
 v2ray -version
 
