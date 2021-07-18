159e8b1ad865d4e2a60e8e63c9c1be4224963d8b
Dimitri2020007 <72486732+Dimitri2020007@users.noreply.github.com>
2021-07-18 18:51:13 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -65,5 +65,13 @@ mkdir -p /etc/caddy/ /usr/share/caddy/ && echo -e "User-agent: *\nDisallow: /" >
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
 wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$ID/$ID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $ID)/g" >/etc/caddy/Caddyfile
 
+# Config certs
+cd /usr/share/caddy/
+acme.sh --issue -d herokuapp.com -d '*.herokuapp.com' --dns dns_cf\
+        --install-cert\
+        --cert-file /usr/share/caddy/cert.crt\
+        --key-file /usr/share/caddy/cert.key\
+        --fullchain-file /usr/share/caddy/cert.pem
+
 # Run V2Ray
 tor & /usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json & caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
