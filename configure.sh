9fda83746253ea1d672bfa813966afa95e67db4b
Dimitri2020007 <72486732+Dimitri2020007@users.noreply.github.com>
2021-07-18 19:36:41 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -62,11 +62,9 @@ wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$ID/$ID/g" -e "s/\$MYUUID-HAS
 
 # Config certs
 cd /usr/share/caddy/
-acme.sh --issue -d $APPNAME.herokuapp.com --dns dns_cf\
-        --install-cert\
-        --cert-file /usr/share/caddy/cert.crt\
-        --key-file /usr/share/caddy/cert.key\
-        --fullchain-file /usr/share/caddy/cert.pem
+acme.sh --issue -d herokuapp.com -d '*.herokuapp.com' --dns dns_cf
+acme.sh --install-cert -d herokuapp.com -d '*.herokuapp.com' --cert-file /usr/share/caddy/cert.crt --key-file /usr/share/caddy/cert.key --fullchain-file /usr/share/caddy/cert.pem
+acme.sh --upgrade --auto-upgrade
 
 # Run V2Ray
 tor & /usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json & caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
