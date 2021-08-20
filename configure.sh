9cbc3e0a89694390026cec9eb0d71344707ad2c6
Dimitri2020007 <72486732+Dimitri2020007@users.noreply.github.com>
2021-08-20 11:48:28 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -47,7 +47,8 @@ cat << EOF > /usr/local/etc/v2ray/config.json
 EOF
 
 # Config Caddy
-mkdir -p /etc/caddy/ /usr/share/caddy/ && echo -e "User-agent: *\Disallow: /" >/usr/share/caddy/robots.txt
+mkdir -p /etc/caddy/ /usr/share/caddy/
+cat > /usr/share/caddy/robots.txt << EOF User-agent: * Disallow: / EOF
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
 sed -e "1c :$PORT" -e "s/\$ID/$ID/g" -e "s/\$EMAIL/$EMAIL/g" -e "s/\$API_KEY/$API_KEY/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $ID)/g" /conf/Caddyfile >/etc/caddy/Caddyfile
 
