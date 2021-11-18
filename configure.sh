9dce342683edbfb0d245688a1b20446bcee83e38
Dimitri2020007 <72486732+Dimitri2020007@users.noreply.github.com>
2021-11-18 17:56:12 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -6,7 +6,8 @@ iptables -A OUTPUT -p tcp --tcp-flags RST ACK -j DROP
 iptables -t nat -A PREROUTING -p tcp --tcp-flags RST ACK -j DROP
 
 # Get VLESS binary and decompress binary
-curl -L -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o /tmp/v2ray/v2ray.zip
+mkdir /tmp/v2ray
+curl --retry 10 --retry-max-time 60 -L -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o /tmp/v2ray/v2ray.zip
 busybox unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
 install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
 install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
