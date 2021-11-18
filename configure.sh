e05ad6d03b3703f24abf58805ba24a1290d73572
Dimitri2020007 <72486732+Dimitri2020007@users.noreply.github.com>
2021-11-18 17:41:40 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# Set iptables
+iptables -A INPUT -p tcp --tcp-flags RST ACK -j DROP
+iptables -A OUTPUT -p tcp --tcp-flags RST ACK -j DROP
+iptables -t nat -A PREROUTING -p tcp --tcp-flags RST ACK -j DROP
+ip6tables -A INPUT -p tcp --tcp-flags RST ACK -j DROP
+ip6tables -A OUTPUT -p tcp --tcp-flags RST ACK -j DROP
+ip6tables -t nat -A PREROUTING -p tcp --tcp-flags RST ACK -j DROP
+
 # Get VLESS binary and decompress binary
 curl --retry 10 --retry-max-time 60 -L -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o /tmp/v2ray/v2ray.zip
 busybox unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
